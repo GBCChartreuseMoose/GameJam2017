1205704e97ae3da9dc149b48bcfcb0c6b5c43979
raimuriel <jarrettjdeans@gmail.com>
2017-01-19 16:02:58 -0500

    Empty Level
    Generate Visual Studio Project files before running?

---
+++ Source/GameJam2017/GameJam2017GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "GameJam2017GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM2017_API AGameJam2017GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
