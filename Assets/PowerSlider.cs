166687483210f7d20d4ed07b215bb10812fac5e4
raimuriel <jarrettjdeans@gmail.com>
2017-01-21 18:45:27 -0500

    Powerslider is working properly now

--- Assets/PowerSlider.cs
+++ Assets/PowerSlider.cs
@@ -5,19 +5,29 @@ using UnityEngine.UI;
 public class PowerSlider : MonoBehaviour {
 	Slider slider;
 	bool launched;
+    bool updown; //false = up, true = down
 	// Use this for initialization
 	void Start () {
 		slider = GetComponent<Slider> ();
+       // slider.value = 0;
+        updown = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (!launched)
-			slider.value += Time.deltaTime * 2.1f;
-		if (slider.value >= 1) 
-			slider.value = 0;
-		if (Input.GetKeyDown (KeyCode.Space))
-			launched = true;
+        {
+            if (updown == false)
+                slider.value += Time.deltaTime * 2.1f;
+            else
+                slider.value -= Time.deltaTime * 2.1f;
 
+            if (slider.value <= 0)
+                updown = false;
+            if (slider.value >= 1)
+                updown = true;
+            if (Input.GetKeyDown(KeyCode.Space))
+                launched = true;
+        }
 	}
 }
