a85722f988036132e6b88929716ccf3cdb012092
Kyle Oram <Kyle__oram@hotmail.com>
2017-01-20 19:52:56 -0500

    SkyScrolling

---
+++ GBCGameJamWaves2017/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour {
+
+	[SerializeField] Transform bg1;
+	[SerializeField] Transform bg2;
+
+	[Range (0.1f,20.0f)] public float scrollSpeed = 5.0f;
+
+	private float dist;
+
+
+	// Use this for initialization
+	void Start () {
+		dist = Mathf.Abs (bg2.position.x - bg1.position.x) - 0.003f;
+		bg2.position = new Vector2 (bg1.position.x+dist, bg2.position.y);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		bg1.Translate(new Vector2(-scrollSpeed*Time.deltaTime, 0));
+		bg2.Translate(new Vector2(-scrollSpeed*Time.deltaTime, 0));
+		Debug.Log (bg1.position.x);
+		if (bg1.position.x <= -21.667f)
+			
+			bg1.position = new Vector2 (bg2.position.x+dist, bg1.position.y);
+		if(bg2.position.x <= -21.667f)
+			bg2.position = new Vector2 (bg1.position.x+dist, bg2.position.y);
+	}
+}
